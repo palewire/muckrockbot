aa33c5b5f6da164429018d7e425135c05362131d
palewire <ben.welsh@gmail.com>
2019-01-26 15:12:45 -0800

    More

--- muckrockbot/management/commands/tweet.py
+++ muckrockbot/management/commands/tweet.py
@@ -7,13 +7,13 @@ class Command(BaseCommand):
     help = "Make tweets at @muckrockbot"
 
     def handle(self, *args, **options):
-        request_list = Request.submitted.untweeted().order_by("-datetime_submitted")[:2]
+        request_list = Request.submitted.untweeted().order_by("-datetime_submitted")[:10]
         for req in request_list:
             print("Tweeting {}".format(req))
             req.post_submission()
             time.sleep(3)
 
-        request_list = Request.completed.untweeted().order_by("-datetime_done")[:2]
+        request_list = Request.completed.untweeted().order_by("-datetime_done")[:10]
         for req in request_list:
             print("Tweeting {}".format(req))
             req.post_completion()
